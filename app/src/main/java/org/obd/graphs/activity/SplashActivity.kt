05d27f8c626708170eb3aef7299c58c20d8e2db7
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-07-29 07:55:18 +0200

    feat: Decrease splash screen to 500ms.

--- app/src/main/java/org/obd/graphs/activity/SplashActivity.kt
+++ app/src/main/java/org/obd/graphs/activity/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-private const val SPLASH_LOAD_TIME = 1000L
+private const val SPLASH_LOAD_TIME = 500L
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
