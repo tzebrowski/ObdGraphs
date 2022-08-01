e46632eff946cf9430d072c9f64d40fc0a958eef
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-08-01 16:31:00 +0200

    refactor: Rename preference datalogger.supported.pids
    Rename preference datalogger.supported.pids to pref.datalogger.supported.pids

--- app/src/main/java/org/obd/graphs/ui/preferences/pid/SupportedPIDs.kt
+++ app/src/main/java/org/obd/graphs/ui/preferences/pid/SupportedPIDs.kt
@@ -2,7 +2,7 @@ package org.obd.graphs.ui.preferences.pid
 
 import org.obd.graphs.ui.preferences.Prefs
 
-const val ECU_SUPPORTED_PIDS = "datalogger.supported.pids"
+const val ECU_SUPPORTED_PIDS = "pref.datalogger.supported.pids"
 
 internal fun updateSupportedPIDsPreference(value: Set<String>) {
     Prefs.edit().putStringSet(ECU_SUPPORTED_PIDS, value).apply()
