578cde3e62a5451fdeae252c464090cfab91bc9c
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-12-31 09:34:00 +0100

    feat: Extend broadcast receivers interface

--- common/src/main/java/org/obd/graphs/Broadcast.kt
+++ common/src/main/java/org/obd/graphs/Broadcast.kt
@@ -2,10 +2,12 @@ package org.obd.graphs
 
 import android.content.Intent
 
-fun sendBroadcastEvent(actionName: String) {
+const val EXTRA_PARAM_NAME = "extra"
+fun sendBroadcastEvent(actionName: String, extra: String = "") {
     getContext()?.run {
         sendBroadcast(Intent().apply {
             action = actionName
+            putExtra(EXTRA_PARAM_NAME, extra)
         })
     }
 }
