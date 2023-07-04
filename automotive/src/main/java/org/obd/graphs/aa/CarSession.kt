75671c64712a3855769616b424fb777f1f82e523
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-07-04 17:57:53 +0200

    feat: AA: Enhance handling of Lifecycle Events in the CarSession

--- automotive/src/main/java/org/obd/graphs/aa/CarSession.kt
+++ automotive/src/main/java/org/obd/graphs/aa/CarSession.kt
@@ -2,10 +2,15 @@ package org.obd.graphs.aa
 
 import android.content.Intent
 import android.content.res.Configuration
+import android.util.Log
 import androidx.car.app.Screen
 import androidx.car.app.Session
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import org.obd.graphs.setCarContext
+
+
+private const val LOG_TAG = "CarSession"
 
 internal class CarSession : Session(), DefaultLifecycleObserver {
     private lateinit var surfaceController: SurfaceController
@@ -18,8 +23,27 @@ internal class CarSession : Session(), DefaultLifecycleObserver {
         return CarScreen(carContext, surfaceController)
     }
 
+
     override fun onCarConfigurationChanged(newConfiguration: Configuration) {
         super.onCarConfigurationChanged(newConfiguration)
         surfaceController.onCarConfigurationChanged()
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        Log.d(LOG_TAG, "Received onResume event")
+        lifecycle.addObserver(surfaceController)
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        Log.d(LOG_TAG, "Received onPause event")
+        lifecycle.removeObserver(surfaceController)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        Log.d(LOG_TAG, "Received onDestroy event")
+        lifecycle.removeObserver(surfaceController)
     }
 }
