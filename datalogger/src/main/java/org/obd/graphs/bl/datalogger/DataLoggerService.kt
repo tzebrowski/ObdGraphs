b69cfe34d5207e08de73858034d1988ed805da48
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-07-16 18:24:00 +0200

    fix: Replace deprecated IntentService by JobIntentService

--- datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerService.kt
+++ datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerService.kt
@@ -1,24 +1,35 @@
 package org.obd.graphs.bl.datalogger
 
-import android.app.IntentService
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.JobIntentService
 import org.obd.graphs.getContext
 
+private const val SCHEDULED_ACTION_START = "org.obd.graphs.logger.scheduled.START"
+private const val SCHEDULED_ACTION_STOP = "org.obd.graphs.logger.scheduled.STOP"
 private const val ACTION_START = "org.obd.graphs.logger.START"
 private const val ACTION_STOP = "org.obd.graphs.logger.STOP"
+private const val SCHEDULED_START_DELAY = "org.obd.graphs.logger.scheduled.delay"
 
-private val jobScheduler = DataLoggerJobScheduler()
+class DataLoggerService : JobIntentService() {
+    private val jobScheduler = DataLoggerJobScheduler()
 
-class DataLoggerService : IntentService("DataLoggerService") {
-
-    override fun onHandleIntent(intent: Intent?) {
-        when (intent?.action) {
+    override fun onHandleWork(intent: Intent) {
+        when (intent.action) {
             ACTION_START -> {
                 dataLogger.start()
             }
             ACTION_STOP -> {
                 dataLogger.stop()
+            }
+
+            SCHEDULED_ACTION_STOP -> {
+                jobScheduler.stop()
+            }
+
+            SCHEDULED_ACTION_START -> {
+                val delay = intent.extras?.getLong(SCHEDULED_START_DELAY)
+                jobScheduler.schedule(delay as Long)
             }
         }
     }
@@ -26,37 +37,42 @@ class DataLoggerService : IntentService("DataLoggerService") {
     companion object {
         @JvmStatic
         fun scheduleStart(delay: Long) {
-            jobScheduler.schedule(delay)
-        }
-
-        @JvmStatic
-        fun scheduledStop() {
-            jobScheduler.stop()
-        }
-
-        @JvmStatic
-        fun start() {
-            getContext()?.let {
-                try {
-                    it.startService(Intent(it, DataLoggerService::class.java).apply {
-                        action = ACTION_START
-                    })
-                }catch (e: IllegalStateException){
-                    Log.e("DataLoggerService", "Failed to start DataLoggerService",e)
-                }
+            enqueueWork(SCHEDULED_ACTION_START) {
+                it.putExtra(SCHEDULED_START_DELAY, delay)
             }
         }
 
         @JvmStatic
+        fun scheduledStop() {
+            enqueueWork(SCHEDULED_ACTION_STOP)
+        }
+
+        @JvmStatic
+        fun start() {
+            enqueueWork(ACTION_START)
+        }
+
+        @JvmStatic
         fun stop() {
-            getContext()?.let {
-                try {
-                    it.startService(Intent(it, DataLoggerService::class.java).apply {
-                        action = ACTION_STOP
-                    })
-                }catch (e: IllegalStateException){
-                    Log.e("DataLoggerService", "Failed to stop DataLoggerService",e)
+            enqueueWork(ACTION_STOP)
+        }
+
+        private fun enqueueWork(intentAction: String, func: (p: Intent) -> Unit = {}) {
+            try {
+                getContext()?.run {
+                    val intent = Intent(this, DataLoggerService::class.java)
+                        .apply { action = intentAction }
+                        .apply { putExtra("init", 1) }
+
+                    func(intent)
+                    enqueueWork(
+                        this,
+                        DataLoggerService::class.java,
+                        1, intent
+                    )
                 }
+            } catch (e: IllegalStateException) {
+                Log.e("DataLoggerService", "Failed to enqueue the work", e)
             }
         }
     }
