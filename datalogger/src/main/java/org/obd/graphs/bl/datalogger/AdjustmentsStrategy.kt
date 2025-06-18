2edda4596cfd7deedea04263d79f8437b0044803
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2025-06-18 21:23:42 +0200

    fix: apply spotless fixes

--- datalogger/src/main/java/org/obd/graphs/bl/datalogger/AdjustmentsStrategy.kt
+++ datalogger/src/main/java/org/obd/graphs/bl/datalogger/AdjustmentsStrategy.kt
@@ -1,4 +1,4 @@
-/**
+ /**
  * Copyright 2019-2025, Tomasz Żebrowski
  *
  * <p>Licensed to the Apache Software Foundation (ASF) under one or more contributor license
@@ -31,134 +31,146 @@ import org.obd.metrics.codec.GeneratorPolicy
 import java.io.File
 
 class AdjustmentsStrategy {
-
-    fun getAdjustments(strategy: QueryStrategyType, preferences: DataLoggerPreferences = dataLoggerPreferences.instance) : Adjustments =
+    fun getAdjustments(
+        strategy: QueryStrategyType,
+        preferences: DataLoggerPreferences = dataLoggerPreferences.instance,
+    ): Adjustments =
         when (strategy) {
             QueryStrategyType.DRAG_RACING_QUERY -> getDragRacingAdjustments(preferences = preferences)
             else -> getDefaultAdjustments(preferences = preferences)
         }
 
-
     private fun getDragRacingAdjustments(preferences: DataLoggerPreferences): Adjustments {
-        var builder = Adjustments.builder()
-            .debugEnabled(preferences.debugLogging)
-            .errorsPolicy(
-                ErrorsPolicy.builder()
-                    .numberOfRetries(preferences.maxReconnectNum)
-                    .reconnectEnabled(preferences.reconnectWhenError).build()
-            )
-            .batchPolicy(
-                BatchPolicy.builder()
-                    .enabled(preferences.batchEnabled)
-                    .responseLengthEnabled(preferences.responseLengthEnabled)
-                    .mode01BatchSize(preferences.mode01BatchSize)
-                    .otherModesBatchSize(preferences.otherModesBatchSize).build()
-            )
-            .collectRawConnectorResponseEnabled(false)
-            .stNxx(
-                STNxxExtensions.builder()
-                    .enabled(dataLoggerPreferences.instance.stnExtensionsEnabled)
-                    .promoteSlowGroupsEnabled(false)
-                    .promoteAllGroupsEnabled(false)
-                    .build()
-            )
-            .vehicleMetadataReadingEnabled(false)
-            .vehicleCapabilitiesReadingEnabled(false)
-            .vehicleDtcReadingEnabled(false)
-            .vehicleDtcCleaningEnabled(false)
-            .cachePolicy(
-                CachePolicy.builder()
-                    .resultCacheEnabled(false).build()
-            )
-            .producerPolicy(
-                ProducerPolicy
-                    .builder()
-                    .pidPriority(0,0) // vehicle speed, rpm
-                    .pidPriority(5,10) // atm pressure, ambient temp
-                    .pidPriority(4,4) // atm pressure, ambient temp
-                    .conditionalSleepEnabled(false)
-                    .build()
-            )
-            .generatorPolicy(
-                GeneratorPolicy
-                    .builder()
-                    .enabled(preferences.generatorEnabled)
-                    .increment(0.5).build()
-            ).adaptiveTimeoutPolicy(
-                AdaptiveTimeoutPolicy
-                    .builder()
-                    .enabled(preferences.adaptiveConnectionEnabled)
-                    .checkInterval(5000)
-                    .commandFrequency(preferences.dragRacingCommandFrequency)
-                    .minimumTimeout(10)
-                    .build()
-            )
+        var builder =
+            Adjustments
+                .builder()
+                .debugEnabled(preferences.debugLogging)
+                .errorsPolicy(
+                    ErrorsPolicy
+                        .builder()
+                        .numberOfRetries(preferences.maxReconnectNum)
+                        .reconnectEnabled(preferences.reconnectWhenError)
+                        .build(),
+                ).batchPolicy(
+                    BatchPolicy
+                        .builder()
+                        .enabled(preferences.batchEnabled)
+                        .responseLengthEnabled(preferences.responseLengthEnabled)
+                        .mode01BatchSize(preferences.mode01BatchSize)
+                        .otherModesBatchSize(preferences.otherModesBatchSize)
+                        .build(),
+                ).collectRawConnectorResponseEnabled(false)
+                .stNxx(
+                    STNxxExtensions
+                        .builder()
+                        .enabled(dataLoggerPreferences.instance.stnExtensionsEnabled)
+                        .promoteSlowGroupsEnabled(false)
+                        .promoteAllGroupsEnabled(false)
+                        .build(),
+                ).vehicleMetadataReadingEnabled(false)
+                .vehicleCapabilitiesReadingEnabled(false)
+                .vehicleDtcReadingEnabled(false)
+                .vehicleDtcCleaningEnabled(false)
+                .cachePolicy(
+                    CachePolicy
+                        .builder()
+                        .resultCacheEnabled(false)
+                        .build(),
+                ).producerPolicy(
+                    ProducerPolicy
+                        .builder()
+                        .pidPriority(0, 0) // vehicle speed, rpm
+                        .pidPriority(5, 10) // atm pressure, ambient temp
+                        .pidPriority(4, 4) // atm pressure, ambient temp
+                        .conditionalSleepEnabled(false)
+                        .build(),
+                ).generatorPolicy(
+                    GeneratorPolicy
+                        .builder()
+                        .enabled(preferences.generatorEnabled)
+                        .increment(0.5)
+                        .build(),
+                ).adaptiveTimeoutPolicy(
+                    AdaptiveTimeoutPolicy
+                        .builder()
+                        .enabled(preferences.adaptiveConnectionEnabled)
+                        .checkInterval(5000)
+                        .commandFrequency(preferences.dragRacingCommandFrequency)
+                        .minimumTimeout(10)
+                        .build(),
+                )
 
-        if (dataLoggerPreferences.instance.stnExtensionsEnabled){
+        if (dataLoggerPreferences.instance.stnExtensionsEnabled) {
             val highPriorityOverridePolicy = PidDefinitionCustomization.builder().priority(0).build()
-            builder = builder
-                .override(Pid.ATM_PRESSURE_PID_ID.id,highPriorityOverridePolicy)
-                .override(Pid.AMBIENT_TEMP_PID_ID.id,highPriorityOverridePolicy)
-                .override(Pid.DYNAMIC_SELECTOR_PID_ID.id,highPriorityOverridePolicy)
-                .override(Pid.ENGINE_TORQUE_PID_ID.id,PidDefinitionCustomization.builder().priority(4).build())
+            builder =
+                builder
+                    .override(Pid.ATM_PRESSURE_PID_ID.id, highPriorityOverridePolicy)
+                    .override(Pid.AMBIENT_TEMP_PID_ID.id, highPriorityOverridePolicy)
+                    .override(Pid.DYNAMIC_SELECTOR_PID_ID.id, highPriorityOverridePolicy)
+                    .override(Pid.ENGINE_TORQUE_PID_ID.id, PidDefinitionCustomization.builder().priority(4).build())
         }
 
         return builder.build()
     }
 
-    private fun getDefaultAdjustments(preferences: DataLoggerPreferences) = Adjustments.builder()
-        .debugEnabled(preferences.debugLogging)
-        .override(Pid.DISTANCE_PID_ID.id, PidDefinitionCustomization.builder().lastInTheQuery(true).build())
-        .formulaExternalParams(FormulaExternalParams.builder().param("unit_tank_size", preferences.fuelTankSize).build())
-        .errorsPolicy(
-            ErrorsPolicy.builder()
-                .numberOfRetries(preferences.maxReconnectNum)
-                .reconnectEnabled(preferences.reconnectWhenError).build()
-        )
-        .batchPolicy(
-            BatchPolicy.builder()
-                .enabled(preferences.batchEnabled)
-                .strictValidationEnabled(preferences.batchStricValidationEnabled)
-                .responseLengthEnabled(preferences.responseLengthEnabled)
-                .mode01BatchSize(preferences.mode01BatchSize)
-                .otherModesBatchSize(preferences.otherModesBatchSize).build()
-        )
-        .collectRawConnectorResponseEnabled(preferences.dumpRawConnectorResponse)
-        .stNxx(
-            STNxxExtensions.builder()
-                .promoteSlowGroupsEnabled(preferences.stnExtensionsEnabled)
-                .promoteAllGroupsEnabled(preferences.stnExtensionsEnabled)
-                .enabled(preferences.stnExtensionsEnabled)
-                .build()
-        )
-        .vehicleMetadataReadingEnabled(preferences.vehicleMetadataReadingEnabled)
-        .vehicleCapabilitiesReadingEnabled(preferences.vehicleCapabilitiesReadingEnabled)
-        .vehicleDtcReadingEnabled(preferences.vehicleDTCReadingEnabled)
-        .vehicleDtcCleaningEnabled(preferences.vehicleDTCCleaningEnabled)
-        .cachePolicy(
-            CachePolicy.builder()
-                .resultCacheFilePath(File(getContext()?.cacheDir, "formula_cache.json").absolutePath)
-                .resultCacheEnabled(preferences.resultsCacheEnabled).build()
-        )
-        .producerPolicy(
-            ProducerPolicy
-                .builder()
-                .conditionalSleepEnabled(preferences.adaptiveConnectionEnabled)
-                .conditionalSleepSliceSize(10).build()
-        )
-        .generatorPolicy(
-            GeneratorPolicy
-                .builder()
-                .enabled(preferences.generatorEnabled)
-                .increment(0.5).build()
-        ).adaptiveTimeoutPolicy(
-            AdaptiveTimeoutPolicy
-                .builder()
-                .enabled(preferences.adaptiveConnectionEnabled)
-                .checkInterval(5000)
-                .commandFrequency(preferences.commandFrequency)
-                .minimumTimeout(10)
-                .build()
-
-        ).build()
+    private fun getDefaultAdjustments(preferences: DataLoggerPreferences) =
+        Adjustments
+            .builder()
+            .debugEnabled(preferences.debugLogging)
+            .override(Pid.DISTANCE_PID_ID.id, PidDefinitionCustomization.builder().lastInTheQuery(true).build())
+            .formulaExternalParams(FormulaExternalParams.builder().param("unit_tank_size", preferences.fuelTankSize).build())
+            .errorsPolicy(
+                ErrorsPolicy
+                    .builder()
+                    .numberOfRetries(preferences.maxReconnectNum)
+                    .reconnectEnabled(preferences.reconnectWhenError)
+                    .build(),
+            ).batchPolicy(
+                BatchPolicy
+                    .builder()
+                    .enabled(preferences.batchEnabled)
+                    .strictValidationEnabled(preferences.batchStricValidationEnabled)
+                    .responseLengthEnabled(preferences.responseLengthEnabled)
+                    .mode01BatchSize(preferences.mode01BatchSize)
+                    .otherModesBatchSize(preferences.otherModesBatchSize)
+                    .build(),
+            ).collectRawConnectorResponseEnabled(preferences.dumpRawConnectorResponse)
+            .stNxx(
+                STNxxExtensions
+                    .builder()
+                    .promoteSlowGroupsEnabled(preferences.stnExtensionsEnabled)
+                    .promoteAllGroupsEnabled(preferences.stnExtensionsEnabled)
+                    .enabled(preferences.stnExtensionsEnabled)
+                    .build(),
+            ).vehicleMetadataReadingEnabled(preferences.vehicleMetadataReadingEnabled)
+            .vehicleCapabilitiesReadingEnabled(preferences.vehicleCapabilitiesReadingEnabled)
+            .vehicleDtcReadingEnabled(preferences.vehicleDTCReadingEnabled)
+            .vehicleDtcCleaningEnabled(preferences.vehicleDTCCleaningEnabled)
+            .cachePolicy(
+                CachePolicy
+                    .builder()
+                    .resultCacheFilePath(File(getContext()?.cacheDir, "formula_cache.json").absolutePath)
+                    .resultCacheEnabled(preferences.resultsCacheEnabled)
+                    .build(),
+            ).producerPolicy(
+                ProducerPolicy
+                    .builder()
+                    .conditionalSleepEnabled(preferences.adaptiveConnectionEnabled)
+                    .conditionalSleepSliceSize(10)
+                    .build(),
+            ).generatorPolicy(
+                GeneratorPolicy
+                    .builder()
+                    .enabled(preferences.generatorEnabled)
+                    .increment(0.5)
+                    .build(),
+            ).adaptiveTimeoutPolicy(
+                AdaptiveTimeoutPolicy
+                    .builder()
+                    .enabled(preferences.adaptiveConnectionEnabled)
+                    .checkInterval(5000)
+                    .commandFrequency(preferences.commandFrequency)
+                    .minimumTimeout(10)
+                    .build(),
+            ).build()
 }
