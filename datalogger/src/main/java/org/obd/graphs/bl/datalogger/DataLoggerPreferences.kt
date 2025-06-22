b7edad8a855b5ed5a914ca2fae6378eaa36543dd
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2025-06-22 15:37:38 +0200

    feat: Refactoring of DataLoggerPreferences creation

--- datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerPreferences.kt
+++ datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerPreferences.kt
@@ -29,42 +29,42 @@ private const val GENERIC_MODE = "Generic mode"
 private const val PREFERENCE_CONNECTION_TYPE = "pref.adapter.connection.type"
 
 data class DataLoggerPreferences(
-    var individualQueryStrategyEnabled: Boolean,
-    var debugLogging: Boolean,
-    var connectionType: String,
-    var tcpHost: String,
-    var wifiSSID: String,
-    var tcpPort: Int,
-    var connectionTimeout: Int,
-    var stnExtensionsEnabled: Boolean,
-    var batchEnabled: Boolean,
-    var batchStricValidationEnabled: Boolean,
-    var otherModesBatchSize: Int?,
-    var mode01BatchSize: Int?,
-    var reconnectWhenError: Boolean,
-    var adapterId: String,
-    var commandFrequency: Long,
-    var dragRacingCommandFrequency: Long,
-    var initDelay: Long,
-    var mode: String,
-    var generatorEnabled: Boolean,
-    var adaptiveConnectionEnabled: Boolean,
-    var resultsCacheEnabled: Boolean,
-    var initProtocol: String,
-    var maxReconnectNum: Int,
-    var resources: Set<String>,
-    var vehicleMetadataReadingEnabled: Boolean,
-    var vehicleCapabilitiesReadingEnabled: Boolean,
-    var vehicleDTCReadingEnabled: Boolean,
-    var vehicleDTCCleaningEnabled: Boolean,
-    var responseLengthEnabled: Boolean,
-    var gracefulStop: Boolean,
-    var dumpRawConnectorResponse: Boolean,
-    var delayAfterReset: Long,
-    var fuelTankSize: Int,
-    var vehicleStatusPanelEnabled: Boolean,
-    var vehicleStatusDisconnectWhenOff: Boolean,
-    var gmeExtensionsEnabled: Boolean
+    var individualQueryStrategyEnabled: Boolean = false,
+    var debugLogging: Boolean = false,
+    var connectionType: String = "bluetooth",
+    var tcpHost: String = "192.168.0.10",
+    var wifiSSID: String = "",
+    var tcpPort: Int = 35000,
+    var connectionTimeout: Int = 2000,
+    var stnExtensionsEnabled: Boolean = false,
+    var batchEnabled: Boolean = true,
+    var batchStricValidationEnabled: Boolean = false,
+    var otherModesBatchSize: Int? = null,
+    var mode01BatchSize: Int? = null,
+    var reconnectWhenError: Boolean = true,
+    var adapterId: String = "OBDII",
+    var commandFrequency: Long = 6,
+    var dragRacingCommandFrequency: Long = 10,
+    var initDelay: Long = 500,
+    var mode: String = GENERIC_MODE,
+    var generatorEnabled: Boolean = false,
+    var adaptiveConnectionEnabled: Boolean = false,
+    var resultsCacheEnabled: Boolean = false,
+    var initProtocol: String = "AUTO",
+    var maxReconnectNum: Int = 0,
+    var resources: Set<String> = modules.getDefaultModules().keys,
+    var vehicleMetadataReadingEnabled: Boolean = true,
+    var vehicleCapabilitiesReadingEnabled: Boolean = true,
+    var vehicleDTCReadingEnabled: Boolean = false,
+    var vehicleDTCCleaningEnabled: Boolean = false,
+    var responseLengthEnabled: Boolean = false,
+    var gracefulStop: Boolean = true,
+    var dumpRawConnectorResponse: Boolean = false,
+    var delayAfterReset: Long = 0,
+    var fuelTankSize: Int = 58,
+    var vehicleStatusPanelEnabled: Boolean =  false,
+    var vehicleStatusDisconnectWhenOff: Boolean = false,
+    var gmeExtensionsEnabled: Boolean = false
 )
 
 
@@ -76,137 +76,95 @@ class DataLoggerPreferencesManager {
             if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
                 Log.v(LOG_TAG, "Key to update $key")
             }
-            instance = loadPreferences()
+            instance = update()
         }
     }
 
     private var strongReference: SharedPreferenceChangeListener = SharedPreferenceChangeListener()
-    var instance: DataLoggerPreferences
+    var instance: DataLoggerPreferences = DataLoggerPreferences()
 
     init {
         Prefs.registerOnSharedPreferenceChangeListener(strongReference)
-        instance = loadPreferences()
+        instance = update()
     }
 
     fun reload() {
-        instance = loadPreferences()
+        instance = update()
     }
 
-    private fun loadPreferences(): DataLoggerPreferences {
-        val connectionType = Prefs.getS(PREFERENCE_CONNECTION_TYPE, "bluetooth")
+    private fun update(): DataLoggerPreferences = instance.apply{
+        connectionType = Prefs.getS(PREFERENCE_CONNECTION_TYPE, "bluetooth")
 
-        val timeout = try {
+        connectionTimeout = try {
             Prefs.getS("pref.adapter.connection.timeout", "2000").toInt()
         } catch (e: Exception) {
             2000
         }
 
-        val stnEnabled = Prefs.getBoolean("pref.adapter.stn.enabled", false)
+        stnExtensionsEnabled = Prefs.getBoolean("pref.adapter.stn.enabled", false)
 
-        val queryForEachViewStrategyEnabled = Prefs.getBoolean("pref.adapter.query.individual.enabled", false)
+        individualQueryStrategyEnabled = Prefs.getBoolean("pref.adapter.query.individual.enabled", false)
 
-        val tcpHost = Prefs.getS("pref.adapter.connection.tcp.host", "192.168.0.10")
+        tcpHost = Prefs.getS("pref.adapter.connection.tcp.host", "192.168.0.10")
 
-        val tcpPort = try {
+        tcpPort = try {
             Prefs.getS("pref.adapter.connection.tcp.port", "35000").toInt()
         } catch (e: Exception) {
             35000
         }
 
-        val wifiSSID = Prefs.getS("pref.adapter.connection.tcp.ssid", "")
+        wifiSSID = Prefs.getS("pref.adapter.connection.tcp.ssid", "")
 
-        val mode22batchSize = Prefs.getString("pref.adapter.batch.size", null)
-        val mode01batchSize = Prefs.getString("pref.adapter.batch_01.size", null)
-        val batchEnabled = Prefs.getBoolean("pref.adapter.batch.enabled", true)
-        val batchStrictValidationEnabled = Prefs.getBoolean("pref.adapter.batch.strict_validation.enabled", false)
+        otherModesBatchSize = Prefs.getString("pref.adapter.batch.size", null)?.toInt()
+        mode01BatchSize = Prefs.getString("pref.adapter.batch_01.size", null)?.toInt()
+        batchEnabled = Prefs.getBoolean("pref.adapter.batch.enabled", true)
+        batchStricValidationEnabled = Prefs.getBoolean("pref.adapter.batch.strict_validation.enabled", false)
 
-        val debugLogging = Prefs.getBoolean("pref.debug.logging.enabled", false)
+        debugLogging = Prefs.getBoolean("pref.debug.logging.enabled", false)
 
-        val reconnectWhenError = Prefs.getBoolean("pref.adapter.reconnect", true)
-        val adapterId = Prefs.getS("pref.adapter.id", "OBDII")
-        val commandFrequency = Prefs.getS("pref.adapter.command.freq", "6").toLong()
-        val dragRacingCommandFrequency = Prefs.getS("pref.drag_race.vehicle_speed.freq", "10").toLong()
+        reconnectWhenError = Prefs.getBoolean("pref.adapter.reconnect", true)
+        adapterId = Prefs.getS("pref.adapter.id", "OBDII")
+        commandFrequency = Prefs.getS("pref.adapter.command.freq", "6").toLong()
+        dragRacingCommandFrequency = Prefs.getS("pref.drag_race.vehicle_speed.freq", "10").toLong()
 
+        initDelay = Prefs.getS("pref.adapter.init.delay", "500").toLong()
+        delayAfterReset = Prefs.getS("pref.adapter.init.delay_after_reset", "0").toLong()
 
-        val initDelay = Prefs.getS("pref.adapter.init.delay", "500").toLong()
-        val delayAfterReset = Prefs.getS("pref.adapter.init.delay_after_reset", "0").toLong()
+        mode = Prefs.getS("pref.mode", GENERIC_MODE)
+        generatorEnabled = Prefs.isEnabled("pref.debug.generator.enabled")
+        adaptiveConnectionEnabled = Prefs.isEnabled("pref.adapter.adaptive.enabled")
+        resultsCacheEnabled = Prefs.isEnabled("pref.adapter.cache.result.enabled")
 
-        val mode = Prefs.getS("pref.mode", GENERIC_MODE)
-        val generatorEnabled = Prefs.isEnabled("pref.debug.generator.enabled")
-        val adaptiveConnectionEnabled = Prefs.isEnabled("pref.adapter.adaptive.enabled")
-        val resultsCacheEnabled = Prefs.isEnabled("pref.adapter.cache.result.enabled")
+        initProtocol = Prefs.getS("pref.adapter.init.protocol", "AUTO")
 
-        val initProtocol = Prefs.getS("pref.adapter.init.protocol", "AUTO")
-
-        val maxReconnectNum = try {
+        maxReconnectNum = try {
             Prefs.getS("pref.adapter.reconnect.max_retry", "0").toInt()
         } catch (e: Exception) {
             0
         }
 
-        val vehicleMetadataReadingEnabled =
+        vehicleMetadataReadingEnabled =
             Prefs.getBoolean("pref.adapter.init.fetchDeviceProperties", true)
 
-        val vehicleCapabilitiesReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchSupportedPids", true)
-        val vehicleDTCReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchDTC", false)
-        val vehicleDTCCleaningEnabled = Prefs.getBoolean("pref.adapter.init.cleanDTC", false)
+        vehicleCapabilitiesReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchSupportedPids", true)
+        vehicleDTCReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchDTC", false)
+        vehicleDTCCleaningEnabled = Prefs.getBoolean("pref.adapter.init.cleanDTC", false)
 
-        val responseLength = Prefs.getBoolean("pref.adapter.responseLength.enabled", false)
+        responseLengthEnabled = Prefs.getBoolean("pref.adapter.responseLength.enabled", false)
 
-        val gracefulStop = Prefs.getBoolean("pref.adapter.graceful_stop.enabled", true)
-        val dumpRawConnectorResponse = Prefs.getBoolean("pref.debug.trip.save.connector_response", false)
+        gracefulStop = Prefs.getBoolean("pref.adapter.graceful_stop.enabled", true)
+        dumpRawConnectorResponse = Prefs.getBoolean("pref.debug.trip.save.connector_response", false)
 
-        val resources  = Prefs.getStringSet(PREF_MODULE_LIST, modules.getDefaultModules().keys)!!
+        resources  = Prefs.getStringSet(PREF_MODULE_LIST, modules.getDefaultModules().keys)!!
 
-        val fuelTankSize = Prefs.getS("pref.vehicle_settings.fuelTankSize", "58").toInt()
-        val vehicleStatusPanel = Prefs.getBoolean("pref.vehicle_settings.vehicle_status_panel_enabled", false)
-        val vehicleStatusDisconnectWhenOff = Prefs.getBoolean("pref.vehicle_settings.disconnect_when_off", false)
-        val gmeExtensionsEnabled = Prefs.getBoolean( "pref.profile.2_0_GME_extension.enabled", false)
-
-        val dataLoggerPreferences = DataLoggerPreferences(
-            vehicleStatusDisconnectWhenOff = vehicleStatusDisconnectWhenOff,
-            vehicleStatusPanelEnabled = vehicleStatusPanel,
-            dragRacingCommandFrequency = dragRacingCommandFrequency,
-            otherModesBatchSize = mode22batchSize?.toInt(),
-            mode01BatchSize = mode01batchSize?.toInt(),
-            stnExtensionsEnabled = stnEnabled,
-            connectionType = connectionType,
-            tcpHost = tcpHost,
-            tcpPort = tcpPort,
-            wifiSSID = wifiSSID,
-            batchEnabled = batchEnabled,
-            reconnectWhenError = reconnectWhenError,
-            adapterId = adapterId,
-            commandFrequency = commandFrequency,
-            initDelay = initDelay,
-            mode = mode,
-            generatorEnabled = generatorEnabled,
-            adaptiveConnectionEnabled = adaptiveConnectionEnabled,
-            resultsCacheEnabled = resultsCacheEnabled,
-            initProtocol = initProtocol,
-            maxReconnectNum = maxReconnectNum,
-            resources = resources,
-            vehicleMetadataReadingEnabled = vehicleMetadataReadingEnabled,
-            vehicleCapabilitiesReadingEnabled = vehicleCapabilitiesReadingEnabled,
-            vehicleDTCReadingEnabled = vehicleDTCReadingEnabled,
-            vehicleDTCCleaningEnabled = vehicleDTCCleaningEnabled,
-            responseLengthEnabled = responseLength,
-            gracefulStop = gracefulStop,
-            connectionTimeout = timeout,
-            dumpRawConnectorResponse = dumpRawConnectorResponse,
-            delayAfterReset = delayAfterReset,
-            debugLogging = debugLogging,
-            individualQueryStrategyEnabled = queryForEachViewStrategyEnabled,
-            batchStricValidationEnabled = batchStrictValidationEnabled,
-            fuelTankSize = fuelTankSize,
-            gmeExtensionsEnabled = gmeExtensionsEnabled
-        )
+        fuelTankSize = Prefs.getS("pref.vehicle_settings.fuelTankSize", "58").toInt()
+        vehicleStatusPanelEnabled = Prefs.getBoolean("pref.vehicle_settings.vehicle_status_panel_enabled", false)
+        vehicleStatusDisconnectWhenOff = Prefs.getBoolean("pref.vehicle_settings.disconnect_when_off", false)
+        gmeExtensionsEnabled = Prefs.getBoolean( "pref.profile.2_0_GME_extension.enabled", false)
 
         if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
             Log.v(LOG_TAG, "Loaded data-logger preferences: $dataLoggerPreferences")
         }
-
-        return dataLoggerPreferences
     }
 }
 
