d8fcdcbf8b8fc10a5b477ef2ca1f694059173bc2
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2025-06-23 19:00:31 +0200

    feat: Do not update all data loggers settings all the time.

--- datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerSettings.kt
+++ datalogger/src/main/java/org/obd/graphs/bl/datalogger/DataLoggerSettings.kt
@@ -1,4 +1,4 @@
- /**
+/**
  * Copyright 2019-2025, Tomasz Żebrowski
  *
  * <p>Licensed to the Apache Software Foundation (ASF) under one or more contributor license
@@ -82,7 +82,7 @@ internal class DataLoggerSettingsManager : SettingsManager {
             if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
                 Log.v(LOG_TAG, "Key to update $key")
             }
-            instance = update()
+            instance = update(key)
         }
     }
 
@@ -91,79 +91,119 @@ internal class DataLoggerSettingsManager : SettingsManager {
 
     init {
         Prefs.registerOnSharedPreferenceChangeListener(strongReference)
-        instance = update()
+        reload()
     }
 
     override fun instance(): DataLoggerSettings = instance
 
     override fun reload() {
-        instance = update()
+        update("pref.adapter.connection.type")
+        update("pref.adapter.connection.timeout")
+        update("pref.adapter.stn.enabled")
+        update("pref.adapter.query.individual.enabled")
+        update("pref.adapter.connection.tcp.host")
+        update("pref.adapter.connection.tcp.port")
+        update("pref.adapter.connection.tcp.ssid")
+        update("pref.adapter.batch.size")
+        update("pref.adapter.batch_01.size")
+        update("pref.adapter.batch.enabled")
+        update("pref.adapter.batch.strict_validation.enabled")
+        update("pref.adapter.reconnect")
+        update("pref.adapter.id")
+        update("pref.adapter.command.freq")
+        update("pref.adapter.init.delay")
+        update("pref.adapter.init.delay_after_reset")
+        update("pref.adapter.cache.result.enabled")
+        update("pref.adapter.init.protocol")
+        update("pref.adapter.reconnect.max_retry")
+        update("pref.adapter.init.fetchSupportedPids")
+        update("pref.adapter.init.fetchDTC")
+        update("pref.adapter.init.cleanDTC")
+        update("pref.adapter.responseLength.enabled")
+        update("pref.adapter.init.fetchDeviceProperties")
+        update("pref.adapter.graceful_stop.enabled")
+        update("pref.adapter.adaptive.enabled")
+        update("pref.debug.logging.enabled")
+        update("pref.drag_race.vehicle_speed.freq")
+        update("pref.mode")
+        update("pref.debug.generator.enabled")
+        update("pref.debug.trip.save.connector_response")
+        update(PREF_MODULE_LIST)
+        update("pref.vehicle_settings.fuelTankSize")
+        update("pref.vehicle_settings.vehicle_status_panel_enabled")
+        update("pref.vehicle_settings.disconnect_when_off")
+        update("pref.profile.2_0_GME_extension.enabled")
     }
 
-    private fun update(): DataLoggerSettings =
+    private fun update(key: String?): DataLoggerSettings =
         instance.apply {
-            adapter.connectionType = Prefs.getS("pref.adapter.connection.type", "bluetooth")
-            adapter.connectionTimeout =
-                try {
-                    Prefs.getS("pref.adapter.connection.timeout", "2000").toInt()
-                } catch (e: Exception) {
-                    2000
+            Log.i(LOG_TAG,"Updating preference: $key")
+            when (key) {
+                "pref.adapter.connection.type" -> adapter.connectionType = Prefs.getS(key, "bluetooth")
+                "pref.adapter.connection.timeout" -> {
+                    adapter.connectionTimeout =
+                        try {
+                            Prefs.getS(key, "2000").toInt()
+                        } catch (e: Exception) {
+                            2000
+                        }
                 }
 
-            adapter.stnExtensionsEnabled = Prefs.getBoolean("pref.adapter.stn.enabled", false)
-            adapter.individualQueryStrategyEnabled = Prefs.getBoolean("pref.adapter.query.individual.enabled", false)
-            adapter.tcpHost = Prefs.getS("pref.adapter.connection.tcp.host", "192.168.0.10")
+                "pref.adapter.stn.enabled" -> adapter.stnExtensionsEnabled = Prefs.getBoolean(key, false)
+                "pref.adapter.query.individual.enabled" -> adapter.individualQueryStrategyEnabled = Prefs.getBoolean(key, false)
+                "pref.adapter.connection.tcp.host" -> adapter.tcpHost = Prefs.getS(key, "192.168.0.10")
+                "pref.adapter.connection.tcp.port" ->
+                    adapter.tcpPort =
+                        try {
+                            Prefs.getS(key, "35000").toInt()
+                        } catch (e: Exception) {
+                            35000
+                        }
 
-            adapter.tcpPort =
-                try {
-                    Prefs.getS("pref.adapter.connection.tcp.port", "35000").toInt()
-                } catch (e: Exception) {
-                    35000
+                "pref.adapter.connection.tcp.ssid" -> adapter.wifiSSID = Prefs.getS(key, "")
+                "pref.adapter.batch.size" -> adapter.otherModesBatchSize = Prefs.getString(key, null)?.toInt()
+                "pref.adapter.batch_01.size" -> adapter.mode01BatchSize = Prefs.getString(key, null)?.toInt()
+                "pref.adapter.batch.enabled" -> adapter.batchEnabled = Prefs.getBoolean(key, true)
+                "pref.adapter.batch.strict_validation.enabled" -> adapter.batchStricValidationEnabled = Prefs.getBoolean(key, false)
+                "pref.adapter.reconnect" -> adapter.reconnectWhenError = Prefs.getBoolean(key, true)
+                "pref.adapter.id" -> adapter.adapterId = Prefs.getS(key, "OBDII")
+                "pref.adapter.command.freq" -> adapter.commandFrequency = Prefs.getS(key, "6").toLong()
+                "pref.adapter.init.delay" -> adapter.initDelay = Prefs.getS(key, "500").toLong()
+                "pref.adapter.init.delay_after_reset" -> adapter.delayAfterReset = Prefs.getS(key, "0").toLong()
+                "pref.adapter.cache.result.enabled" -> adapter.resultsCacheEnabled = Prefs.isEnabled(key)
+                "pref.adapter.init.protocol" -> adapter.initProtocol = Prefs.getS(key, "AUTO")
+                "pref.adapter.reconnect.max_retry" ->
+                    adapter.maxReconnectNum =
+                        try {
+                            Prefs.getS(key, "0").toInt()
+                        } catch (e: Exception) {
+                            0
+                        }
+
+                "pref.adapter.init.fetchSupportedPids" -> adapter.vehicleCapabilitiesReadingEnabled = Prefs.getBoolean(key, true)
+                "pref.adapter.init.fetchDTC" -> adapter.vehicleDTCReadingEnabled = Prefs.getBoolean(key, false)
+                "pref.adapter.init.cleanDTC" -> adapter.vehicleDTCCleaningEnabled = Prefs.getBoolean(key, false)
+                "pref.adapter.responseLength.enabled" -> adapter.responseLengthEnabled =  Prefs.getBoolean(key, false)
+                "pref.adapter.init.fetchDeviceProperties" -> adapter.vehicleMetadataReadingEnabled = Prefs.getBoolean(key, true)
+                "pref.adapter.graceful_stop.enabled" -> adapter.gracefulStop = Prefs.getBoolean(key, true)
+                "pref.adapter.adaptive.enabled" -> adapter.adaptiveConnectionEnabled = Prefs.isEnabled(key)
+                "pref.debug.logging.enabled" -> debugLogging = Prefs.getBoolean(key, false)
+                "pref.drag_race.vehicle_speed.freq" -> dragRacingCommandFrequency = Prefs.getS(key, "10").toLong()
+                "pref.mode" -> mode = Prefs.getS(key, "Generic mode")
+                "pref.debug.generator.enabled" -> generatorEnabled = Prefs.isEnabled(key)
+                "pref.debug.trip.save.connector_response" -> dumpRawConnectorResponse = Prefs.getBoolean(key, false)
+                "pref.pids.registry.list" -> resources = Prefs.getStringSet(key, modules.getDefaultModules().keys)!!
+                "pref.vehicle_settings.fuelTankSize" -> fuelTankSize = Prefs.getS(key, "58").toInt()
+                "pref.vehicle_settings.vehicle_status_panel_enabled" -> vehicleStatusPanelEnabled = Prefs.getBoolean(key, false)
+                "pref.vehicle_settings.disconnect_when_off" -> vehicleStatusDisconnectWhenOff = Prefs.getBoolean(key, false)
+                "pref.profile.2_0_GME_extension.enabled" -> gmeExtensionsEnabled = Prefs.getBoolean(key, false)
+                else -> {
+                    Log.w(LOG_TAG, "Received unknown preference: $key")
                 }
-
-            adapter.wifiSSID = Prefs.getS("pref.adapter.connection.tcp.ssid", "")
-            adapter.otherModesBatchSize = Prefs.getString("pref.adapter.batch.size", null)?.toInt()
-            adapter.mode01BatchSize = Prefs.getString("pref.adapter.batch_01.size", null)?.toInt()
-            adapter.batchEnabled = Prefs.getBoolean("pref.adapter.batch.enabled", true)
-            adapter.batchStricValidationEnabled = Prefs.getBoolean("pref.adapter.batch.strict_validation.enabled", false)
-            adapter.reconnectWhenError = Prefs.getBoolean("pref.adapter.reconnect", true)
-            adapter.adapterId = Prefs.getS("pref.adapter.id", "OBDII")
-            adapter.commandFrequency = Prefs.getS("pref.adapter.command.freq", "6").toLong()
-            adapter.initDelay = Prefs.getS("pref.adapter.init.delay", "500").toLong()
-            adapter.delayAfterReset = Prefs.getS("pref.adapter.init.delay_after_reset", "0").toLong()
-            adapter.resultsCacheEnabled = Prefs.isEnabled("pref.adapter.cache.result.enabled")
-            adapter.initProtocol = Prefs.getS("pref.adapter.init.protocol", "AUTO")
-
-            adapter.maxReconnectNum =
-                try {
-                    Prefs.getS("pref.adapter.reconnect.max_retry", "0").toInt()
-                } catch (e: Exception) {
-                    0
-                }
-
-            adapter.vehicleCapabilitiesReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchSupportedPids", true)
-            adapter.vehicleDTCReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchDTC", false)
-            adapter.vehicleDTCCleaningEnabled = Prefs.getBoolean("pref.adapter.init.cleanDTC", false)
-            adapter.responseLengthEnabled = Prefs.getBoolean("pref.adapter.responseLength.enabled", false)
-            adapter.vehicleMetadataReadingEnabled = Prefs.getBoolean("pref.adapter.init.fetchDeviceProperties", true)
-            adapter.gracefulStop = Prefs.getBoolean("pref.adapter.graceful_stop.enabled", true)
-            adapter.adaptiveConnectionEnabled = Prefs.isEnabled("pref.adapter.adaptive.enabled")
-
-            debugLogging = Prefs.getBoolean("pref.debug.logging.enabled", false)
-            dragRacingCommandFrequency = Prefs.getS("pref.drag_race.vehicle_speed.freq", "10").toLong()
-            mode = Prefs.getS("pref.mode", "Generic mode")
-            generatorEnabled = Prefs.isEnabled("pref.debug.generator.enabled")
-            dumpRawConnectorResponse = Prefs.getBoolean("pref.debug.trip.save.connector_response", false)
-            resources = Prefs.getStringSet(PREF_MODULE_LIST, modules.getDefaultModules().keys)!!
-            fuelTankSize = Prefs.getS("pref.vehicle_settings.fuelTankSize", "58").toInt()
-            vehicleStatusPanelEnabled = Prefs.getBoolean("pref.vehicle_settings.vehicle_status_panel_enabled", false)
-            vehicleStatusDisconnectWhenOff = Prefs.getBoolean("pref.vehicle_settings.disconnect_when_off", false)
-            gmeExtensionsEnabled = Prefs.getBoolean("pref.profile.2_0_GME_extension.enabled", false)
-
-            if (Log.isLoggable(LOG_TAG, Log.VERBOSE)) {
-                Log.v(LOG_TAG, "Loaded data-logger preferences: $dataLoggerSettings")
             }
         }
+
+
 }
 
 val dataLoggerSettings: SettingsManager by lazy { DataLoggerSettingsManager() }
