f9e352e0df40bce1921cfd09c3c540ae7e6840ba
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2026-02-25 12:28:06 +0100

    feat: apply spotless fixes

--- datalogger/src/main/java/org/obd/graphs/bl/query/QueryStrategy.kt
+++ datalogger/src/main/java/org/obd/graphs/bl/query/QueryStrategy.kt
@@ -1,4 +1,4 @@
-/**
+ /**
  * Copyright 2019-2026, Tomasz Żebrowski
  *
  * <p>Licensed to the Apache Software Foundation (ASF) under one or more contributor license
@@ -21,7 +21,6 @@ import java.io.Serializable
 internal open class QueryStrategy(
     protected val pids: MutableSet<Long> = mutableSetOf(),
 ) : Serializable {
-
     open fun update(newPIDs: Set<Long>) {
         pids.clear()
         pids.addAll(newPIDs)
